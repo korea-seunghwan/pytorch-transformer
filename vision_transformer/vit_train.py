f4e4564f39fbdccd17255ce4496fd8b78b0e4a48
korea-seunghwan <senghan1992@korea.ac.kr>
2021-03-24 10:33:47 +0000

    seq2seq tutorial 30:39

--- vision_transformer/vit_train.py
+++ vision_transformer/vit_train.py
@@ -17,10 +17,10 @@ transform = transforms.Compose(
     transforms.Normalize((0.5, 0.5, 0.5), (0.5,0.5,0.5))]
 )
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data'), train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=4)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data'), train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=2)
 
 classes = ['plane', 'car', 'bird', 'cat','deer','dog','frog','horse','ship','truck']
