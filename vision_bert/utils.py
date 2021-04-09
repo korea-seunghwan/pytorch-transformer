4971533657d522d767560f58a014dee9a80e1898
korea-seunghwan <senghan1992@korea.ac.kr>
2021-04-09 11:16:19 +0000

    vision bert

---
+++ vision_bert/utils.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+import pandas as pd
+
+dat = pd.read_csv('/data/bsh/datas/cifar-10/trainLabels.csv')
+label = []
+for row_index, row in dat.iterrows():
+    label.append(row.iloc[1:]['label'])
+    if row_index > 5:
+        break
+    print(row.iloc[1:])
+
+file_index = 0
+for file in os.listdir('/data/bsh/datas/cifar-10/train'):
+    if '.png' in file:
+        shutil.move('/data/bsh/datas/cifar-10/train/' + file, '/data/bsh/datas/cifar-10/train/'+label[file_index] + '/' + file)
+        # print(file)
+        file_index += 1
