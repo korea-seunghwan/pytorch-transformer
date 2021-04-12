a38315477064b6662eeeac4dc7f7da8ddf4d4473
korea-seunghwan <senghan1992@korea.ac.kr>
2021-04-12 06:13:52 +0000

    cifar-10 test하기 위한 파일 정리

--- vision_bert/utils.py
+++ vision_bert/utils.py
@@ -7,10 +7,11 @@ dat = pd.read_csv('/data/bsh/datas/cifar-10/trainLabels.csv')
 label = []
 for row_index, row in dat.iterrows():
     label.append(row.iloc[1:]['label'])
-    if row_index > 5:
-        break
-    print(row.iloc[1:])
+    # if row_index > 5:
+    #     break
+    # print(row.iloc[1:])
 
+# print(label)
 file_index = 0
 for file in os.listdir('/data/bsh/datas/cifar-10/train'):
     if '.png' in file:
--- vision_bert/vib.py
+++ vision_bert/vib.py
@@ -8,16 +8,29 @@ import torch.nn as nn
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 
-from transformers import BertForTokenClassification
+from transformers import BertTokenizer, BertForTokenClassification
 
 from PIL import Image
 import numpy as np
 
-dataset = dset.ImageFolder(root="/data/bsh/datas/cifar-10/train/",
-                transform=transforms.Compose([transforms.ToTensor()]))
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertForTokenClassification.from_pretrained('bert-base-uncased')
 
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True, num_workers=8)
+image = np.asfarray(Image.open('/data/bsh/datas/cifar-10/train/airplane/9992.png'))
+image = image.reshape(-1)
+image = torch.from_numpy(image).long()
+image = image.flatten()
+print(image)
 
-for i, data in enumerate(dataloader):
-    print(data[0])
-    print(data[1])
+# inputs = tokenizer.build_inputs_with_special_tokens(image).tolist()
+outputs1, outputs2 = model(image.unsqueeze(0))
+print(outputs1)
+print(outputs2)
+
+# dataset = dset.ImageFolder(root="/data/bsh/datas/cifar-10/train/")
+
+# dataloader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True, num_workers=8)
+
+# for i, data in enumerate(dataloader):
+#     print(data[0])
+#     print(data[1])
